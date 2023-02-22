cb5e4cd2c79a1c5ce87aa6d223eadc23ae0b5aee
pequetc <pequetc@gmail.com>
2023-02-22 15:09:33 +0100

    fixed the bug which broke the previous version, added looping

--- main.py
+++ main.py
@@ -30,20 +30,20 @@ temperature = input()
 
 driver = webdriver.Chrome()
 reference_density = get_density(pressure, temperature)
-print(f"The density at {pressure} bar and {temperature} celsius degrees is {reference_density} kilograms per cubic metre")
+print(
+    f"The density at {pressure} bar and {temperature} celsius degrees is {reference_density} kilograms per cubic meter")
 
 print("Please enter the initial temperature")
 init_temperature = input()
 init_pressure = 10
 init_density = get_density(init_pressure, init_temperature)
+print(f"The search for desired density will start from {init_pressure} bar with increment/decrement of 5")
 
 cond = "y"
 while cond:
     print(f"Found value of density is now {init_density}")
+    print(f"Program is trying to find pressure at which density is {reference_density}")
     print("Do you wish to keep searching? If no, please press Enter, otherwise press any other key")
-    increment = 5
-    print(f"The pressure will start from 10 bar and increment by {increment} (which is equal 5 by default). Do you "
-          f"wish to enter another value of increment?")
     cond = input()
     if not cond:
         break
@@ -52,3 +52,6 @@ while cond:
     elif init_density > reference_density:
         init_pressure -= 5
     init_density = get_density(init_pressure, init_temperature)
+
+print(f"The found value of pressure is {init_pressure} bar at which the density is {init_density}, which was selected "
+      f"by user as close enough to desired value of {reference_density}")
